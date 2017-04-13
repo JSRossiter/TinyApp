575c9cac7c053b108e95d23f709eb7617b5adabe
JSRossiter <jeffrey.rossiter@me.com>
2017-04-13 01:18:04 +0000

    complete functionality with view tracking

--- express-server.js
+++ express-server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
+const methodOverride = require('method-override');
 const PORT = process.env.PORT || 8080; // default port 8080
 const users = {
   "userRandomID": {
@@ -10,27 +11,34 @@ const users = {
     email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "test": {
+  "test": {
     id: "test",
     email: "1@1",
     password: bcrypt.hashSync("1", 10)
   }
 }
 const urlDatabase = {
-  "b2xVn2": {
-    shortURL: "b2xVn2",
+  "b2xy52": {
+    shortURL: "b2xy52",
     longURL: "http://www.lighthouselabs.ca",
-    userID: "test"
+    userID: "test",
+    totalViews: 0,
+    uniqueViews: 0,
+    viewLog: [{
+      timestamp: 1492038283873,
+      visitor_id: "g56egr"
+      }]
   },
-  "9sm5xK": {
-    shortURL: "9sm5xK",
+  "9sm99K": {
+    shortURL: "9sm99K",
     longURL: "http://www.google.com",
-    userID: "test"
-  },
-  "9s11xK": {
-    shortURL: "9s11xK",
-    longURL: "http://www.notgoogle.com",
-    userID: "nottest"
+    userID: "test",
+    totalViews: 0,
+    uniqueViews: 0,
+    viewLog: [{
+      timestamp: 1492038283879,
+      visitor_id: "85mjgr"
+      }]
   }
 };
 
@@ -42,7 +50,10 @@ app.use(cookieSession({
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
 
 // generates random 6 character alphanumeric string
 function generateRandomString() {
@@ -69,112 +80,6 @@ function findUser(email) {
   return Object.values(users).find(u => u.email === email);
 }
 
-// home page handler
-app.get("/", (req, res) => {
-  if (req.session.user_id) res.redirect("/urls");
-  else res.render("landing_page");
-});
-
-// register
-app.post("/register", (req, res, next) => {
-  if (!req.body.email || !req.body.password) {
-    next({status: 409, message: 'missing email or password'});
-    return;
-  }
-  if(findUser(req.body.email)) {
-    next({status: 409, message: 'user already exists'});
-    return;
-  }
-  userID = generateRandomString();
-  users[userID] = {
-    id: userID,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 10)
-  };
-  req.session.user_id = userID;
-  res.redirect("/urls");
-});
-
-// login
-app.post("/login", (req, res, next) => {
-  if (!req.body.email || !req.body.password) {
-    next({status: 409, message: 'missing email or password'});
-    return;
-  }
-  let user = findUser(req.body.email);
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    req.session.user_id = user.id;
-    res.redirect('/urls');
-  } else {
-    next({status: 409, message: 'bad credentials'});
-  }
-});
-
-// logout
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/");
-});
-
-// follow a link
-app.get("/u/:shortURL", (req, res, next) => {
-  // check if link is in database
-  if (!urlDatabase[req.params.shortURL]) {
-    next({status: 400, message: 'link not in database'});
-    return;
-  }
-  res.redirect(301, urlDatabase[req.params.shortURL].longURL);
-});
-
-// TODO implement/test this
-function checkLogin (req, res, next) {
-  if (!req.session.user_id) {
-    let templateVars = { user: users[req.session.user_id], path: req.path }
-    res.status(401).render("require_login", templateVars);
-    return;
-    // return next({status: 401, message: 'login required'});
-  }
-  next();
-}
-
-// primary views
-app.get("/urls", checkLogin, (req, res) => {
-  let templateVars = { urls: urlsForUser(req.session.user_id), user: users[req.session.user_id] };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", checkLogin, (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  res.render("urls_new", templateVars);
-});
-
-app.get("/urls/:id", checkLogin, (req, res) => {
-  let templateVars = { user: users[req.session.user_id], shortURL: req.params.id, longURL: urlDatabase[req.params.id].longURL };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/register", (req, res) => {
-  let templateVars = { user: users[req.session.user_id] }
-  res.render("register", templateVars);
-});
-
-app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.session.user_id] }
-  res.render("login", templateVars);
-});
-
-// file requests
-app.get("/public/:path/:name", (req, res) => {
-  let fileName = req.params.name;
-  let options = {
-    root: __dirname + '/public/' + req.params.path,
-    dotfiles: 'deny'
-  }
-  res.sendFile(fileName, options, (err) => {
-    if(err) console.log(err);
-  });
-});
-
 // require links to begin with http(s)
 function addProtocol (req, res, next) {
   if (!req.body.longURL.match(/^http(s?):\/\//)) {
@@ -183,28 +88,148 @@ function addProtocol (req, res, next) {
   next();
 }
 
-// create a link
-app.post("/urls", checkLogin, addProtocol, (req, res) => {
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = {
-    shortURL: shortURL,
-    longURL: req.body.longURL,
-    userID: req.session.user_id
-  };
-  res.redirect("/urls");
+// check if link is in database
+function checkLink (req, res, next) {
+  if (!urlDatabase[req.params.shortURL]) {
+    next({status: 400, message: 'link not in database'});
+    return;
+  }
+  next();
+}
+
+// update view log and counters
+function trackView (req, res, next) {
+  let shortURL = req.params.shortURL;
+  if (!req.session.visitor_id) {
+    req.session.visitor_id = generateRandomString();
+  }
+  let log = urlDatabase[shortURL].viewLog;
+  let test = false;
+  for (var i = 0; i < log.length; i++) {
+    if (log[i].visitor_id === req.session.visitor_id) test = true;
+  }
+  if (!test) urlDatabase[shortURL].uniqueViews += 1;
+  urlDatabase[shortURL].totalViews += 1;
+  urlDatabase[shortURL].viewLog.push({
+    timestamp: Date.now(),
+    visitor_id: req.session.visitor_id
+  })
+  next();
+}
+
+// home page handler
+app.get("/", (req, res) => {
+  if (req.session.user_id) res.redirect("/urls");
+  else res.render("landing_page");
 });
 
-// update a link
-app.post("/urls/:id", checkLogin, addProtocol, (req, res) => {
-  urlDatabase[req.params.id].longURL = req.body.longURL;
-  res.redirect("/urls");
+// logout
+app.post("/logout", (req, res) => {
+  req.session.user_id = null;
+  res.redirect("/");
 });
 
-// delete a link
-app.post("/urls/:id/delete", checkLogin, (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+// follow a link
+app.get("/u/:shortURL", checkLink, trackView, (req, res) => {
+  res.redirect(302, urlDatabase[req.params.shortURL].longURL);
 });
+
+function checkLogin (req, res, next) {
+  if (!req.session.user_id) {
+    let templateVars = { user: null, path: req.path }
+    res.status(401).render("require_login", templateVars);
+    return;
+  }
+  next();
+}
+
+app.get("/urls/new", checkLogin, (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  res.render("urls_new", templateVars);
+});
+
+app.route("/urls")
+  .get(checkLogin, (req, res) => {
+    let templateVars = { urls: urlsForUser(req.session.user_id), user: users[req.session.user_id] };
+    res.render("urls_index", templateVars);
+  })
+  // create a link
+  .post(checkLogin, addProtocol, (req, res) => {
+    let shortURL = generateRandomString();
+    urlDatabase[shortURL] = {
+      shortURL: shortURL,
+      longURL: req.body.longURL,
+      userID: req.session.user_id,
+      totalViews: 0,
+      uniqueViews: 0,
+      viewLog: []
+    };
+    res.redirect("/urls");
+  });
+
+app.route("/urls/:id")
+  .get(checkLogin, (req, res) => {
+    let templateVars = {
+      user: users[req.session.user_id],
+      url: urlDatabase[req.params.id]
+    };
+    res.render("urls_show", templateVars);
+  })
+  // update a link
+  .put(checkLogin, addProtocol, (req, res) => {
+    urlDatabase[req.params.id].longURL = req.body.longURL;
+    res.redirect("/urls");
+  })
+  // delete a link
+  .delete(checkLogin, (req, res) => {
+    delete urlDatabase[req.params.id]
+    res.redirect("/urls");
+  });
+
+app.route("/register")
+  .get((req, res) => {
+    let templateVars = { user: users[req.session.user_id] }
+    res.render("register", templateVars);
+  })
+  .post((req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+      next({status: 409, message: 'missing email or password'});
+      return;
+    }
+    if(findUser(req.body.email)) {
+      next({status: 409, message: 'user already exists'});
+      return;
+    }
+    userID = generateRandomString();
+    users[userID] = {
+      id: userID,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10)
+    };
+    req.session.user_id = userID;
+    res.redirect("/urls");
+  });
+
+app.route("/login")
+  .get((req, res) => {
+    let templateVars = { user: users[req.session.user_id] }
+    res.render("login", templateVars);
+  })
+  .post((req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+      next({status: 409, message: 'missing email or password'});
+      return;
+    }
+    let user = findUser(req.body.email);
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      req.session.user_id = user.id;
+      res.redirect('/urls');
+    } else {
+      next({status: 409, message: 'bad credentials'});
+    }
+  });
+
+
 
 // TODO error handling > render a view
 app.use((error, req, res, next) => {
